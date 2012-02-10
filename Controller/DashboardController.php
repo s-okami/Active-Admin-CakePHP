1762567947d7e622f0c1a1edd83b99c15f010729
TeckniX <lokitek@gmail.com>
2012-02-10 16:21:54 -0500

    continued controller updates

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -1,15 +1,19 @@
 <?php
+App::uses('AppController', 'Controller');
+/**
+ * Dashboard Controller
+ *
+ * @property Dashboard $Dashboard
+ */
 class DashboardController extends ActiveAdminAppController {
 
-	var $name = 'Dashboard';
-	var $helpers = array('Html', 'Form');
-	var $uses = array();
-	
-	function admin_index() {
-		$this->redirect('dashboard');
-	}
-	
-	function admin_dashboard() {
-		
-	}
+    var $name = 'Dashboard';
+
+    function admin_index() {
+        $this->redirect('dashboard');
+    }
+
+    function admin_dashboard() {
+
+    }
 }
