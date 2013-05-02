f7386b40d0fc333ef96f71d09f16c1e11ce4a035
s.okami <s.okami@PC-150.local>
2013-05-02 16:35:22 +0900

    fix below error on running ./Console/cake ActiveAdmin.resource Posts
    Notice Error: Undefined index: Dashboard in [app/Plugin/ActiveAdmin/Console/Command/ResourceShell.php, line 18]

--- Console/Command/ResourceShell.php
+++ Console/Command/ResourceShell.php
@@ -1,32 +1,46 @@
 <?php
 class ResourceShell extends AppShell {
+    
     public $uses = array('ActiveAdmin.Dashboard');
     
     public function main() {
+        
         if (empty($this->args)){
+            /*
+                HELP
+            */
             $this->out('Usage: ActiveAdmin.resource [MyControllerName] or [Plugin.MyControllerName]');
             $this->out('eg. ./Console/cake ActiveAdmin.resource Posts');
+            
         } else {
-            foreach($this->args as $myController){
+            foreach ($this->args as $myController) {
                 $myDash = $this->Dashboard->findByValue($myController);
-                if(!empty($myDash)){
+                if (!empty($myDash)) {
                     $this->out("$myController is already part of the dashboard menu");
-                }else{
-                    
+                } else {
                     $myDash = $this->Dashboard->find('first',array('order'=>'Dashboard.display_order DESC'));
-                    $this->Dashboard->create(array('Dashboard'=>array(
-                                                    "name" => "nav_menu",
-                                                    "value" => $myController,
-                                                    "description" => "$myController controller menu",
-                                                    "type" => "controller",
-                                                    "display_order" => $myDash['Dashboard']['display_order']+1
-                                       )));
+                    $display_order = 1;
+                    if (isset($myDash['Dashboard']) && isset($myDash['Dashboard']['display_order'])) {
+                        $display_order = $myDash['Dashboard']['display_order'] + 1;
+                    }
+                    
+                    $this->Dashboard->create(array(
+                        'Dashboard' => array(
+                            "name" => "nav_menu",
+                            "value" => $myController,
+                            "description" => "$myController controller menu",
+                            "type" => "controller",
+                            "display_order" => $display_order
+                        )
+                    ));
+                    
                     if($this->Dashboard->save()){
                         $this->out("Added $myController to the dashboard");
                     }
                 }
             }
         }
+        
         $this->out('Currently installed Dashboard links:');
         $menu = $this->Dashboard->find('all',array('condition'=>array('Dashboard.name' => 'nav_menu')));
         foreach($menu as $item){
